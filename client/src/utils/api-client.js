1facb85d545e396706e306c2432ae3084bd529f0
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-09-28 21:38:14 -0700

    initial commit

---
+++ client/src/utils/api-client.js
@@ -0,0 +1,21 @@
+export function authenticate() {}
+
+export async function signoutUser() {}
+
+export async function updateUser() {}
+
+export async function addVideoView() {}
+
+export async function addComment() {}
+
+export async function addVideo() {}
+
+export async function toggleSubscribeUser() {}
+
+export async function likeVideo() {}
+
+export async function dislikeVideo() {}
+
+export async function deleteVideo() {}
+
+export async function deleteComment() {}
