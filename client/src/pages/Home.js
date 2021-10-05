70081f70c3373ea705cb9e01d4049616ea672a12
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-10-04 21:59:09 -0700

    created 'Home' query, fetch video data

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useQuery } from "react-query";
+import ErrorMessage from "../components/ErrorMessage";
+import HomeSkeleton from "../skeletons/HomeSkeleton";
 import Wrapper from "../styles/Home";
 import VideoGrid from "../styles/VideoGrid";
+import { client } from "../utils/api-client";
 
 function Home() {
+  const { data: videos, isLoading, isError, error } = useQuery("Home", () => client.get('/videos').then(res => res.data.videos));
+
+  if (isLoading) return <HomeSkeleton />
+  if (isError) return <ErrorMessage error={error}/>
+
+  console.log(videos);
+
   return (
     <Wrapper>
       <VideoGrid>Recommended videos</VideoGrid>
