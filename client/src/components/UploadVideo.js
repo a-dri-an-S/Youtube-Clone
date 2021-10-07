2af4969f46f62463a9dede93dfa637354d9c305c
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-10-06 20:52:32 -0700

    created upload video modal

--- client/src/components/UploadVideo.js
+++ client/src/components/UploadVideo.js
@@ -1,12 +1,22 @@
-import React from "react";
+import path from 'path';
+import React, { useState } from "react";
 import { useSnackbar } from "react-simple-snackbar";
 import { uploadMedia } from "../utils/upload-media";
 import { UploadIcon } from "./Icons";
+import UploadVideoModal from "./UploadVideoModal";
 
 function UploadVideo() {
+  const [showModal, setShowModal] = useState(false);
+  const [previewVideo, setPreviewVideo] = useState('');
+  const [thumbnail, setThumbnail] = useState('');
+  const [defaultTitle, setDefaultTitle] = useState('');
+  const [url, setUrl] = useState('');
   const [openSnackbar] = useSnackbar();
 
+  const closeModal = () => setShowModal(false);
+
   async function handleUploadVideo(event) {
+    event.persist();
     const file = event.target.files[0];
 
     if (file) {
@@ -16,12 +26,21 @@ function UploadVideo() {
         return openSnackbar('Video file should be less than 50MB');
       }
 
+      const previewVideo = URL.createObjectURL(file);
+      setPreviewVideo(previewVideo);
+      setShowModal(true);
       const url = await uploadMedia({
         type: "video",
         file,
         preset: "vqawuu0a"
       });
-      console.log(url);
+
+      const defaultTitle = path.basename(file.name, path.extname(file.name));
+      setDefaultTitle(defaultTitle);
+      const extension = path.extname(url);
+      setThumbnail(url.replace(extension, 'jpg'));
+      setUrl(url);
+      event.target.value = "";
     }
   }
 
@@ -37,6 +56,15 @@ function UploadVideo() {
         accept="video/*"
         onChange={handleUploadVideo}
       />
+      {showModal && (
+        <UploadVideoModal 
+          previewVideo={previewVideo}
+          thumbnail={thumbnail}
+          defaultTitle={defaultTitle}
+          url={url}
+          closeModal={closeModal}
+        />
+      )}
     </div>
   );
 }
--- client/src/components/UploadVideoModal.js
+++ client/src/components/UploadVideoModal.js
@@ -1,36 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../styles/Button";
 import Wrapper from "../styles/UploadVideoModal";
 import { CloseIcon } from "./Icons";
 import VideoPlayer from "./VideoPlayer";
 
-function UploadVideoModal() {
-  const [tab, setTab] = React.useState("PREVIEW");
+function UploadVideoModal({ previewVideo, thumbnail, url, defaultTitle, closeModal }) {
+  const [tab, setTab] = useState("PREVIEW");
+  const [title, setTitle] = useState(defaultTitle);
+  const [description, setDescription] = useState("");
+
+  function handleTab() {
+    
+  }
 
   return (
     <Wrapper>
       <div className="modal-content">
         <div className="modal-header">
           <div className="modal-header-left">
-            <CloseIcon />
-            <h3>Video Uploaded!</h3>
+            <CloseIcon onClick={closeModal}/>
+            <h3>{url ? "Video Uploaded" : "Uploading..."}</h3>
           </div>
-          <div style={{ display: "block" }}>
-            <Button>{tab === "PREVIEW" ? "Next" : "Upload"}</Button>
+          <div style={{ display: url ? "block" : "none" }}>
+            <Button onClick={handleTab}>{tab === "PREVIEW" ? "Next" : "Upload"}</Button>
           </div>
         </div>
 
         {tab === "PREVIEW" && (
           <div className="tab video-preview">
-            <VideoPlayer />
+            <VideoPlayer previewUrl={previewVideo} video={url}/>
           </div>
         )}
 
         {tab === "FORM" && (
           <div className="tab video-form">
             <h2>Video Details</h2>
-            <input type="text" placeholder="Enter your video title" />
-            <textarea placeholder="Tell viewers about your video" />
+            <input type="text" placeholder="Enter your video title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+            <textarea placeholder="Tell viewers about your video" value={description} onChange={(e) => setDescription(e.target.value) }/>
           </div>
         )}
       </div>
