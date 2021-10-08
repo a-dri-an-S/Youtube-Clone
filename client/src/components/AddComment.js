f1f9a1c9b6d632e1b6526eb1f2b90c326f6c954a
ADD RIAN <adrian.salinas.1986@gmail.com>
2021-10-07 22:02:46 -0700

    created addComment + handleAddComment func to add comment

--- client/src/components/AddComment.js
+++ client/src/components/AddComment.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSnackbar } from "react-simple-snackbar";
 import defaultAvatar from "../assets/default-avatar.png";
 import { useAuth } from "../context/auth-context";
 import Wrapper from "../styles/CommentList";
+import { addComment } from "../utils/api-client";
 import CommentList from "./CommentList";
 
 function AddComment({ video }) {
   const user = useAuth();
+  const [openSnackbar] = useSnackbar();
+  const [comment, setComment] = useState('');
+
+  function handleAddComment(event) {
+    if (event.keyCode === 13) {
+      event.target.blur();
+
+      if (!comment.trim()) {
+        return openSnackbar("Please write a comment")
+      }
+
+      addComment({ video, comment })
+        .then(() => setComment(""))
+        .catch(() => openSnackbar('Sign in to add a comment'))
+    }
+  }
+
   return (
     <Wrapper>
       <h3>{video.comments.length} comments</h3>
@@ -16,7 +35,13 @@ function AddComment({ video }) {
         ) : (
           <img src={defaultAvatar} alt="default user" />
         )}
-        <textarea placeholder="Add a public comment..." value="" rows={1} />
+        <textarea 
+          placeholder="Add a public comment..." 
+          value={comment}
+          onKeyDown={handleAddComment}
+          onChange={event => setComment(event.target.value)}
+          rows={1}
+        />
       </div>
 
       <CommentList comments={video.comments}/>
